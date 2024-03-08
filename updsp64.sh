1308d6f1ca3c1bc237f4ba8f4280b3b45e12b179
isk727 <isk727@gmail.com>
2024-03-09 01:52:20 +0900

    Add files via upload

---
+++ updsp64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+base='base='/usr/share/momo-2022.4.1_raspberry-pi-os_armv8/html/'
+git='https://raw.githubusercontent.com/isk727/momosp/main/'
+today=`date "+%Y%m%d%H%M%S"`
+wget ${git}stylev.css
+chmod 777 stylev.css
+sudo mv ${base}css/stylev.css ${base}css/stylev.css.${today}
+sudo mv stylev.css ${base}css/stylev.css
+echo 'Update is completed!'
