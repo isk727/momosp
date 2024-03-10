0dc36335d8d829d10a4d0f61586683a9af73d710
isk727 <isk727@gmail.com>
2024-03-11 04:40:35 +0900

    Update updsp64.sh

--- updsp64.sh
+++ updsp64.sh
@@ -3,11 +3,11 @@ base='/usr/share/momo-2022.4.1_raspberry-pi-os_armv8/html/'
 git='https://raw.githubusercontent.com/isk727/momosp/main/'
 today=`date "+%Y%m%d%H%M%S"`
 wget ${git}scriptv.js
-chmod 777 scriptv.css
+chmod 777 scriptv.js
 wget ${git}stylev.css
 chmod 777 stylev.css
-sudo mv ${base}js/scriptv.css ${base}css/scriptv.css.${today}
-sudo mv scriptv.css ${base}css/scriptv.css
+sudo mv ${base}js/scriptv.js ${base}js/scriptv.js.${today}
+sudo mv scriptv.js ${base}js/scriptv.js
 sudo mv ${base}css/stylev.css ${base}css/stylev.css.${today}
 sudo mv stylev.css ${base}css/stylev.css
 echo 'Update is completed!'
